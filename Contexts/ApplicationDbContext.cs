06de3aec305e1d629f0244381aade33fc1adac80
Kevin Britten <kevinjbritten@gmail.com>
2024-10-06 17:31:25 -0400

    remove onconfiguring from ApplicationDbContext

--- Contexts/ApplicationDbContext.cs
+++ Contexts/ApplicationDbContext.cs
@@ -5,9 +5,6 @@ namespace AdoptionHub.Contexts;
 
 public partial class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext()
-    {
-    }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -28,9 +25,6 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<Veterinarian> Veterinarians { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=AdoptionHubDB;user=root;password=12345678", ServerVersion.Parse("8.0.30-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
