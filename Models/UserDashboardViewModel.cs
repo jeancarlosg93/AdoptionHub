580fc1fc7b3f4cefaf9fb7c4ee86c80a7875ce86
jeanc <jeancarlosg93@gmail.com>
2024-10-20 21:57:52 -0400

    Add details page, dashboard, and refactor layout
    Introduced a new details page for viewing individual pets and a dashboard page to list all pets. Refactored the layout for better navigation and updated the home page with a new call-to-action banner. Included minor CSS enhancements for improved responsiveness.

---
+++ Models/UserDashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Mysqlx.Datatypes;
+
+namespace AdoptionHub.Models;
+
+public class UserDashboardViewModel
+{
+    public int Id { get; set; }
+    public String Name { get; set; }
+    public String Breed { get; set; }
+
+    public String Gender { get; set; }
+
+    public String Age { get; set; }
+    public String Temperament { get; set; }
+
+    public String ImagePath { get; set; }
+}
