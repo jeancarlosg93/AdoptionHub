c4859b1afd8197839d20fa4be20f8b47b0356d68
DESKTOP-PNMJLC1\uribe <daianabolini@hotmail.com>
2024-10-29 15:46:06 -0400

    filters progress1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -57,9 +57,49 @@ namespace AdoptionHub.Controllers
             
           return View(pet);
         }
-        
-        
 
+
+        [HttpPost]
+        public IActionResult Search(string species, string breed, string gender)
+        {
+            var filteredPets = GetFilteredPets(species, breed, gender);
+            return PartialView("_PetCards", filteredPets);
+        }
+
+        private List<UserDashboardViewModel> GetFilteredPets(string species = null, string breed = null, string gender = null)
+        {
+            // Query the pets with optional filters
+            var petsQuery = _context.Pets.Include(pet => pet.Details).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(species))
+            {
+                petsQuery = petsQuery.Where(p => p.Details.Species == species);
+            }
+            if (!string.IsNullOrWhiteSpace(breed))
+            {
+                petsQuery = petsQuery.Where(p => p.Details.Breed == breed);
+            }
+            if (!string.IsNullOrWhiteSpace(gender))
+            {
+                petsQuery = petsQuery.Where(p => p.Details.Gender == gender);
+            }
+
+            var filteredPets = petsQuery.Select(p => new UserDashboardViewModel
+            {
+                Id = p.Id,
+                Name = p.Details.Name,
+                Breed = p.Details.Breed,
+                Gender = p.Details.Gender == "F" ? "Female" : "Male",
+                Age = p.Details.DateOfBirth.HasValue
+                    ? (DateTime.Now.Year - p.Details.DateOfBirth.Value.Year).ToString()
+                    : "Unknown",
+                Temperament = p.Details.Temperament,
+            }).ToList();
+
+            return filteredPets;
+        }
+
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/PetSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace AdoptionHub.Models
+{
+    public class PetSearchViewModel
+    {
+        public string? Species { get; set; } 
+        public string? Breed { get; set; } 
+        public string? Age { get; set; } 
+        public string? Size { get; set; } 
+        public string? Gender { get; set; }
+
+        public List<PetDetail> Pets { get; set; }
+    }
+}
