ada4ded495c30f44fec3f448cf1307302c39c43e
DESKTOP-PNMJLC1\uribe <daianabolini@hotmail.com>
2024-11-06 18:11:16 -0500

    updates in adoption form controller

--- Controllers/AdoptionFormController.cs
+++ Controllers/AdoptionFormController.cs
@@ -22,36 +22,47 @@ namespace AdoptionHub.Controllers
             return View();
         }
 
+        //public IActionResult Form(int petId)
+        //{
+        //    var model = new AdoptionFormViewModel();
+        //    ViewBag.PetId = petId;
+        //    return View(model);
+        //}
         public IActionResult Form(int petId)
         {
-            var model = new AdoptionFormViewModel();
-            ViewBag.PetId = petId;
+            var model = new AdoptionFormViewModel { PetId = petId };
             return View(model);
         }
-    }
 
-    [HttpPost]
-    public IActionResult SubmitApplication(AdoptionFormViewModel model)
-    {
-        var pet = _context.AdoptionApplications.First(pet => pet.Id == model.Id);
 
-        var adoptionApplication = new Adoptionapplication
+        [HttpPost]
+        public IActionResult SubmitApplication(AdoptionFormViewModel model)
         {
-            FirstName = model.FirstName,
-            LastName = model.LastName,
-            Email = model.Email,
-            Address = model.Address,
-            PhoneNumber = model.PhoneNumber,
-            Comments = model.Comments,
-            PetId = model.PetId,
-            ApplicationDateTime = DateTime.Now,
-            ApplicationStatus = "Pending"  //default status
-        };
+            var pet = _context.Adoptionapplications.FirstOrDefault(p => p.Id == model.PetId);
 
-        _context.Adoptionapplications.Add(adoptionApplication);
-        _context.SaveChanges();
+            if (pet == null)
+            {
+                ModelState.AddModelError("", "Pet not found.");
+                return View("Form", model);
+            }
 
-        return RedirectToAction();
-        //("ApplicationConfirmation", new { id = adoptionApplication.Id });
+            var adoptionApplication = new Adoptionapplication
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                Address = model.Address,
+                PhoneNumber = model.PhoneNumber,
+                Comments = model.Comments,
+                PetId = model.PetId,
+                ApplicationDateTime = DateTime.Now,
+                ApplicationStatus = "Pending"  //default status
+            };
+
+            _context.Adoptionapplications.Add(adoptionApplication);
+            _context.SaveChanges();
+
+            return RedirectToAction("ApplicationConfirmation", new { id = adoptionApplication.Id });
+        }
     }
 }
